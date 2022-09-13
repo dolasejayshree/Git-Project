0f1da6dbd78885f400bb82ce3ed1e53f4f8f61ac
dolase jayshree <dolasejayshree823@gmail.com>
2022-09-13 16:34:09 +0530

    first commit

---
+++ MainProject/src/com/collection/ArrayListDemo.java
@@ -0,0 +1,24 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("Poonm");
+		al.add("Priyanka");
+		al.add("Rutuja");
+		al.add("Vaishali");
+		System.out.println(al);
+		
+		Iterator<String> itr=al.iterator();
+		{
+			while(itr.hasNext());
+			System.out.println(itr.next());
+		}
+		
+	}
+
+}
